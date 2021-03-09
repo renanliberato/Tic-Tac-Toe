7043ded4597d62fc3a48ed98a4b3ce3793aff39f
Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
2021-03-09 18:05:32 -0300

    Added server implementation

---
+++ MyGameServer/Hubs/GameHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace MyGameServer.Hubs
+{
+    public class GameHub : Hub
+    {
+        public GameHub()
+        {
+        }
+
+        public async Task ConnectToMatch(string matchId) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, matchId);
+            await Clients.Client(Context.ConnectionId).SendAsync("OnConnectedToMatch");
+        }
+
+        public async Task DisconnectFroMatch(string matchId) {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, matchId);
+        }
+
+        public async Task SendEventToOtherMatchClients(string match, object @event) {
+            await Clients.Group(match).SendAsync("OnSendEventToOtherMatchClients", @event);
+        }
+    }
+}
